9ecffe2237e59a05ec61cc94e9b7c28b9af5c477
ikechu-star <ikechukwuotis949@gmail.com>
2025-02-14 14:29:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '2348086434394', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
